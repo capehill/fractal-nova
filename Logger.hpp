46fd1a8ce945ad1e1edadc1c90c13aab01bf29b1
capehill <juha.niemimaki@gmail.com>
2021-03-13 15:29:13 +0200

    Cleanup

--- Logger.hpp
+++ Logger.hpp
@@ -8,8 +8,4 @@ bool IsVerbose();
 void Log(const char * fmt, ...) __attribute__ ((format (printf, 1, 2)));
 void Error(const char * fmt, ...) __attribute__ ((format (printf, 1, 2)));
 
-//void logLine(const char * fmt, ...) __attribute__ ((format (printf, 1, 2)));
-//void logAlways(const char * fmt, ...) __attribute__ ((format (printf, 1, 2)));
-//void logDebug(const char * fmt, ...) __attribute__ ((format (printf, 1, 2)));
-
 } // logging
